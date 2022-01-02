04011c99c8f05adf73bd3e56b0543a65ac5edbcf
monukumar98 <monu012as@gmail.com>
2022-01-02 14:36:03 +0530

    OOPS-1

---
+++ Lec25/PersionClient.java
@@ -0,0 +1,17 @@
+package Lec25;
+
+public class PersionClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persion p = new Persion();
+//		int [] arr = new int [3];
+//		System.out.println(arr[-1]);
+		p.setAge(-10);
+		p.setName("kaju");
+		System.out.println(p.getAge()+" "+p.getName());
+		
+
+	}
+
+}
