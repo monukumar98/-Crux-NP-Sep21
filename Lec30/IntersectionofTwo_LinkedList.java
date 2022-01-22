be32b49425c353db48980af78592f033c052da30
monukumar98 <monu012as@gmail.com>
2022-01-22 19:06:34 +0530

    LinkedListQps

--- Lec30/IntersectionofTwo_LinkedList.java
+++ Lec30/IntersectionofTwo_LinkedList.java
@@ -12,4 +12,35 @@ public class IntersectionofTwo_LinkedList {
 			val = x;
 		}
 	}
+	
+	public class Solution {
+	    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+	    	
+	    	 ListNode A = headA;
+	         ListNode B = headB;
+	      
+	         while(A!=B){
+	             if(A!=null){
+	                 A=A.next;
+	             }
+	             else{
+	                
+	                A=headB; 
+	             }
+	             
+	             if(B!=null){
+	                 
+	                 B=B.next;
+	                 
+	             }
+	             else{
+	                
+	               B=headA;  
+	             }
+	             
+	         }
+	         return A;
+	        
+	    }
+	}
 }
