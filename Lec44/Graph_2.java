e75cfde72567bbd3787bccb7432ffedf8c4d6bd9
monukumar98 <monu012as@gmail.com>
2022-03-06 12:10:21 +0530

    Graph

---
+++ Lec44/Graph_2.java
@@ -0,0 +1,164 @@
+package Lec44;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Graph_2 {
+	HashMap<Integer, HashMap<Integer, Integer>> map;
+
+	public Graph_2(int v) {
+		// TODO Auto-generated constructor stub
+		map = new HashMap<>();
+		for (int i = 1; i <= v; i++) {
+			map.put(i, new HashMap<>());
+
+		}
+
+	}
+
+	public void addedge(int v1, int v2, int cost) {
+		map.get(v1).put(v2, cost);
+		map.get(v2).put(v1, cost);
+	}
+
+	public void BFT() {
+		HashSet<Integer> visited = new HashSet<>();
+		LinkedList<Integer> queue = new LinkedList<>();
+
+		for (int src : map.keySet()) {
+			if (visited.contains(src)) {
+				continue;
+			}
+			queue.add(src);
+			while (!queue.isEmpty()) {
+				// remove
+				int rv = queue.remove();
+
+				// Ignore if already visited
+				if (visited.contains(rv)) {
+					continue;
+				}
+				// print
+				System.out.print(rv + " ");
+				// marked visited
+				visited.add(rv);
+				// nbrs
+				for (int key : map.get(rv).keySet()) {
+					if (!visited.contains(key)) {
+						queue.add(key);
+					}
+
+				}
+
+			}
+		}
+		System.out.println();
+
+	}
+
+	public void DFT() {
+		HashSet<Integer> visited = new HashSet<>();
+		Stack<Integer> stack = new Stack<>();
+		for (int src : map.keySet()) {
+			if (visited.contains(src)) {
+				continue;
+			}
+			stack.push(src);
+			while (!stack.isEmpty()) {
+				// remove
+				int rv = stack.pop();
+
+				// Ignore if already visited
+				if (visited.contains(rv)) {
+					continue;
+				}
+				// print
+				System.out.print(rv + " ");
+				// marked visited
+				visited.add(rv);
+				// nbrs
+				for (int key : map.get(rv).keySet()) {
+					if (!visited.contains(key)) {
+						stack.push(key);
+					}
+
+				}
+
+			}
+		}
+		System.out.println();
+
+	}
+
+	public boolean IsCycle() {
+		HashSet<Integer> visited = new HashSet<>();
+		LinkedList<Integer> queue = new LinkedList<>();
+
+		for (int src : map.keySet()) {
+			if (visited.contains(src)) {
+				continue;
+			}
+			queue.add(src);
+			while (!queue.isEmpty()) {
+				// remove
+				int rv = queue.remove();
+
+				// Ignore if already visited
+				if (visited.contains(rv)) {
+					return true;
+				}
+
+				// marked visited
+				visited.add(rv);
+				// nbrs
+				for (int key : map.get(rv).keySet()) {
+					if (!visited.contains(key)) {
+						queue.add(key);
+					}
+
+				}
+
+			}
+		}
+		return false;
+
+	}
+
+	public boolean IsConnected() {
+		HashSet<Integer> visited = new HashSet<>();
+		LinkedList<Integer> queue = new LinkedList<>();
+		int count = 0;
+		for (int src : map.keySet()) {
+			if (visited.contains(src)) {
+				continue;
+			}
+			count++;
+			queue.add(src);
+			while (!queue.isEmpty()) {
+				// remove
+				int rv = queue.remove();
+
+				// Ignore if already visited
+				if (visited.contains(rv)) {
+					continue;
+				}
+
+				// marked visited
+				visited.add(rv);
+				// nbrs
+				for (int key : map.get(rv).keySet()) {
+					if (!visited.contains(key)) {
+						queue.add(key);
+					}
+
+				}
+
+			}
+		}
+		return count == 1;
+
+	}
+
+}
---
+++ Lec44/Graph_2_Clinet.java
@@ -0,0 +1,24 @@
+package Lec44;
+
+
+
+public class Graph_2_Clinet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Graph_2 g = new Graph_2(7);
+		g.addedge(1, 4, 5);
+		g.addedge(1, 2, 10);
+		g.addedge(2, 3, 7);
+		g.addedge(3, 4, 2);
+		g.addedge(4, 5, 4);
+		g.addedge(5, 6, 3);
+		g.addedge(5, 7, 2);
+		g.addedge(6, 7, 7);
+		g.BFT();
+		g.DFT();
+		System.out.println(g.IsCycle());
+		System.out.println(g.IsConnected());
+	}
+
+}
